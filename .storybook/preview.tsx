0ba5e69ecd235a0e1eb8b66eb772ad640caa8139
Manu Rana <manurana@hotmail.com>
2023-07-16 15:09:59 +0930

    added decorator to preview, and renamed to preview.tsx

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,5 +1,7 @@
 import type { Preview } from "@storybook/react";
 import "../src/app/globals.css";
+import { sora } from "../src/lib/fonts";
+import * as React from "react";
 
 const preview: Preview = {
   parameters: {
@@ -11,6 +13,14 @@ const preview: Preview = {
       },
     },
   },
+  decorators: [
+    (Story) => (
+      <div className={`${sora.variable}`}>
+        {/* 👇 Decorators in Storybook also accept a function. Replace <Story/> with Story() to enable it  */}
+        <Story />
+      </div>
+    ),
+  ],
 };
 
 export default preview;
